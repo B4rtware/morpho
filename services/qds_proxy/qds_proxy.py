608026577f6612b1c64dc05304c95a7ec9fa62d6
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-01-28 00:42:44 +0100

    refactor(qds_proxy): implement eureka functionality

--- services/qds_proxy/qds_proxy.py
+++ services/qds_proxy/qds_proxy.py
@@ -1,14 +1,12 @@
 from dataclasses import dataclass
-import dataclasses
 from pathlib import Path
 import sys
-import json
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 import grpc
 import py_eureka_client.eureka_client as eureka_client
 
 sys.path.append(str(Path(".").resolve()))
-
+from dtslog import log
 from dtaservice.dtaservice_pb2 import TransformDocumentResponse
 from dtaservice.dtaservice_pb2_grpc import DTAServerStub
 from services.services import DTAServer
@@ -16,28 +14,60 @@ from services.services import DTAServer
 
 class QDS_PROXY(DTAServer):
     version = "0.0.1"
-    app_name = "QDS.PROXY"
+    app_name = "DE.TU-BERLIN.QDS.PROXY"
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.registered_proxy_app_instances = []
 
     def work(self, request, context) -> Tuple[str, Optional[str]]:
         # TODO: database call here
         pass
 
     def TransformDocument(self, request, context):
-        #to = eureka_client.get_application("http://localhost:8761/eureka", request.service_name)
-        print(f"wants to call: {request.service_name}")
+        # TODO: implement internal service communication protocol using grpc (sendEvent("register_proxy", "..."))
+        # let services be registed by the proxy to route to it self
+        if (
+            request.document.decode()
+            == "REGISTER ME - id:59e46078-6ca5-4f0b-9732-e6fdf5f5a49e"
+        ):
+            self.registered_proxy_app_instances.append(
+                eureka_client.init_registry_client(
+                    eureka_server=self.dts.RegistrarURL,
+                    instance_id=self.dts.HostName,
+                    app_name=request.service_name + ".PROXY",
+                    instance_port=int(self.dts.PortToListen),
+                    instance_secure_port_enabled=self.dts.IsSSL,
+                    metadata={"DTA-Type": "PROXY_SERVICE"},
+                )
+            )
+            log.info(f"registered {request.service_name + '.PROXY'}")
+            return TransformDocumentResponse(
+                trans_document="OK - id:59e46078-6ca5-4f0b-9732-e6fdf5f5a49e".encode()
+            )
+
+        # TODO: consider using discovery client for caching
+        to = eureka_client.get_application(
+            "http://localhost:8761/eureka", request.service_name
+        )
+        log.info(f"wants to call: {request.service_name}")
+        # remove self from the instance list
+        # instances: List[eureka_client.Instance] = [
+        #     instance
+        #     for instance in to.instances
+        #     if instance.port.port != self.dts.PortToListen
+        # ]
+        instance = to.instances[0]
         # forward message
-        channel = grpc.insecure_channel("localhost:50052")
+        channel = grpc.insecure_channel(f"{instance.ipAddr}:{instance.port.port}")
         stub = DTAServerStub(channel)
         result = stub.TransformDocument(request)
-        # forward request to database service
-        channel = grpc.insecure_channel("localhost:50053")
-        stub = DTAServerStub(channel)
-        result = stub.TransformDocument(request)
-        print(result.trans_document)
         return TransformDocumentResponse(
             trans_document=result.trans_document,
             trans_output=result.trans_output,
-            error=result.error
+            error=result.error,
         )
 
-QDS_PROXY.run(50051)
+
+QDS_PROXY.run()
