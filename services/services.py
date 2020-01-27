69a9732a68bb189f933bc7a71829333f15e0f70f
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-01-28 00:44:05 +0100

    refactor(services): add new parameters from go repo

--- services/services.py
+++ services/services.py
@@ -12,53 +12,32 @@ import dtaservice.doctransserver as pb
 import dtaservice.dtaservice_pb2_grpc as dtaservice_pb2_grpc
 import dtaservice.dtaservice_pb2 as dtaservice_pb2
 
-parser = argparse.ArgumentParser()
-parser.add_argument_group("Registrar")
-parser.add_argument("--RegistrarURL", type=str, help="Registry URL")
-parser.add_argument(
-    "--RegistrarUser", type=str, help="Registry User, no user used if not provided"
-)
-# parser.add_argument("--RegistrarPwd-pwd",  type=str, help="Registry User Password, no password used if not provided")
-parser.add_argument(
-    "--TTL", type=int, help="Time in seconds to reregister at Registrar."
-)
-parser.add_argument_group("Service")
-parser.add_argument(
-    "--HostName",
-    type=str,
-    help="If provided will be used as hostname, else automatically derived.",
-)
-parser.add_argument(
-    "--AppName", type=str, help="ID of the service as e.g. 'DOC.TXT.COUNT.'"
-)
-parser.add_argument(
-    "--PortToListen", type=str, help="On which port to listen for this service."
-)
-parser.add_argument(
-    "--DtaType",
-    type=str,
-    help="One of Gateway or Service. Service is assumed if not provided.",
-)
-parser.add_argument("--IsSSL", type=str, help="Can the service be reached via SSL.")
-parser.add_argument_group("Generic")
-parser.add_argument(
-    "--LogLevel",
-    type=int,
-    help="Log level, one of panic, fatal, error, warn or warning, info, debug, trace",
-)
-parser.add_argument("--CfgFile", type=str, help="The config file to use")
-parser.add_argument(
-    "--Init",
-    help="Create a default config file as defined by cfg-file, if set. If not set ~/.dta/<AppName>/config.json will be created.",
-    action="store_true",
-)
-parser.add_argument(
-    "--Trace",
-    help="Create Proxy for capturing traces from and to this microservice",
-    action="store_true",
-)
 
-class DTAServer(ABC, dtaservice_pb2_grpc.DTAServerServicer):
+parser = argparse.ArgumentParser()
+# fmt: off
+parser.add_argument_group("Registrar")
+parser.add_argument("--Register", action="store_true", help="Register service with EUREKA, if set")
+parser.add_argument("--RegistrarURL", type=str, help="Registry URL")
+parser.add_argument("--RegistrarUser", type=str, help="Registry User, no user used if not provided")
+parser.add_argument("--RegistrarPwd",  type=str, help="Registry User Password, no password used if not provided")
+parser.add_argument("--TTL", type=int, help="Time in seconds to reregister at Registrar.")
+
+parser.add_argument_group("Service")
+parser.add_argument("--HostName", type=str, help="If provided will be used as hostname, else automatically derived.")
+parser.add_argument("--AppName", type=str, help="ID of the service as e.g. 'DOC.TXT.COUNT.'")
+parser.add_argument("--PortToListen", type=str, help="On which port to listen for this service.")
+parser.add_argument("--DtaType", type=str, help="One of Gateway or Service. Service is assumed if not provided.")
+parser.add_argument("--IsSSL", type=str, help="Can the service be reached via SSL.")
+parser.add_argument("--REST", action="store_true", help="REST-API enabled on port 80, if set.")
+parser.add_argument("--HTTPPort", type=str, help="On which httpPort to listen for REST, if enableREST is set. Ignored otherwise.")
+
+parser.add_argument_group("Generic")
+parser.add_argument("--LogLevel", type=str, help="Log level, one of panic, fatal, error, warn or warning, info, debug, trace", default="INFO", choices=("CRITICAL", "FATAL", "ERROR", "WARNING", "WARN", "INFO", "DEBUG", "NOTSET"))
+parser.add_argument("--CfgFile", type=str, help="The config file to use")
+parser.add_argument("--Init", help="Create a default config file as defined by cfg-file, if set. If not set ~/.dta/<AppName>/config.json will be created.", action="store_true")
+parser.add_argument("--Trace", help="Connect Proxy for capturing traces to this microservice", action="store_true")
+# fmt: on
+
 
     @abstractmethod
     def work(self, request, context) -> Tuple[str, Optional[str]]:
