f457d0e877ea4864425cd341f54ec62c2cd6cc7b
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-01-28 00:57:04 +0100

    refactor(qds_database): remove custom implemenation of a database layer

--- services/qds_database/qds_database.py
+++ services/qds_database/qds_database.py
@@ -1,91 +1,28 @@
 from pathlib import Path
 import sys
-import sqlite3
-import datetime
-import waitress
-from threading import Thread
-import falcon
-from dataclasses import dataclass
-import dataclasses
-from multiprocessing import Process
-from typing import Tuple, Optional
-import json
 
 sys.path.append(str(Path(".").resolve()))
-
-from dtaservice.dtaservice_pb2 import TransformDocumentResponse
-from dtaservice.dtaservice_pb2_grpc import DTAServerStub
 from services.services import DTAServer
 
-db_path = Path(__file__).parent / Path("traces.db")
+import doctrans_py_swagger
 
+from typing import Tuple, Optional
 
-@dataclass
-class Trace:
-    created: str
-    app_name: str
-    is_unittest: bool
-
-
-class Traces(object):
-    def on_get(self, req, resp):
-        conn = sqlite3.connect(db_path.absolute())
-        c = conn.cursor()
-
-        res = c.execute("SELECT * FROM traces").fetchall()
-
-        # print(res.fetchall())
-        # fast serialize
-        traces = [dataclasses.asdict(Trace(*r)) for r in res]
-
-        resp.status = falcon.HTTP_200
-        resp.body = json.dumps(traces, indent=4)
-
-    def on_put(self, req, resp):
-        conn = sqlite3.connect(db_path.absolute())
-        c = conn.cursor()
-
-        print(req)
-
-
-
-app = falcon.API()
-traces = Traces()
-app.add_route("/traces", traces)
-
-
+api_instance = doctrans_py_swagger.TracesApi(doctrans_py_swagger.ApiClient())
+    
 class QDS_DATABASE(DTAServer):
-    def __init__(self):
-        print("setup database")
-        db_exists = db_path.exists()
-        self.conn = sqlite3.connect(db_path.absolute(), check_same_thread=False)
-
-        if not db_exists:
-            c = self.conn.cursor()
-            c.execute(
-                """CREATE TABLE traces (created text, app_name text, is_unittest int)"""
-            )
-
-        print("start trace server")
-        self.web_thread = Thread(target=waitress.serve, args=(app,), daemon=True)
-        self.web_thread.start()
-        print("still running")
-
     def work(self, request, context) -> Tuple[str, Optional[str]]:
-        c = self.conn.cursor()
-        cur_datetime = datetime.datetime.now().isoformat()
-        print(f"INSERT INTO traces VALUES ({cur_datetime}, {request.service_name}, 0)")
-        c.execute(
-            f"INSERT INTO traces VALUES ('{cur_datetime}', '{request.service_name}', 0)"
+        trace = doctrans_py_swagger.Trace(
+            service_name=""
         )
 
-        self.conn.commit()
+        try:
+            api_response = api_instance.traces_create(trace)
+            print(api_response)
+        except ApiException as e:
+            # TODO: log this output
+            print(f"Exception when calling TracesApi->traces_create: {e}\n")
 
-        return ("Added new database entry.", None)
-
-    def __del__(self):
-        self.web_thread.join()
-        print("closed web thread")
-
+        return (request.document, None)
 
 QDS_DATABASE.run(50053)
