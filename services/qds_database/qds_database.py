c22b7d0564deed3080acf0168bde5d9df15c1db2
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-01-28 02:46:42 +0100

    refactor(qds_database): use swagger api

--- services/qds_database/qds_database.py
+++ services/qds_database/qds_database.py
@@ -9,20 +9,23 @@ import doctrans_py_swagger
 from typing import Tuple, Optional
 
 api_instance = doctrans_py_swagger.TracesApi(doctrans_py_swagger.ApiClient())
-    
+
+
 class QDS_DATABASE(DTAServer):
+    version = "0.0.1"
+    app_name = "DE.TU-Berlin.QDS.DATABASE"
+
     def work(self, request, context) -> Tuple[str, Optional[str]]:
-        trace = doctrans_py_swagger.Trace(
-            service_name=""
-        )
+        trace = doctrans_py_swagger.Trace(service_name="")
 
         try:
             api_response = api_instance.traces_create(trace)
             print(api_response)
-        except ApiException as e:
+        except doctrans_py_swagger.rest.ApiException as e:
             # TODO: log this output
             print(f"Exception when calling TracesApi->traces_create: {e}\n")
 
-        return (request.document, None)
+        return ("", None)
 
-QDS_DATABASE.run(50053)
+
+QDS_DATABASE.run()
