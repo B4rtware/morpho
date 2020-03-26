3f5f6f1cd8b44f96106d2427c0e8904a70d1b97e
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-03-26 15:11:56 +0100

    refactor(qds_echo): adapt typing changes from services

--- examples/services/qds_count/qds_count.py
+++ examples/services/qds_count/qds_count.py
@@ -1,30 +1,39 @@
 from dataclasses import dataclass
 import dataclasses
+import json
 from pathlib import Path
 import sys
-import json
 from typing import Optional, Tuple
+
+from grpc import ServicerContext
 
 sys.path.append(str(Path(".").resolve()))
 
-from dtaservice.server import DTAServer
+from service.proto.dtaservice_pb2 import DocumentRequest
+from service.server import DTAServer
+
 
 @dataclass
-class CountResults():
+class CountResults:
     Bytes: int
     Lines: int
     Words: int
+
 
 class QDS_COUNT(DTAServer):
     version = "0.0.1"
     app_name = "QDS.ECHO"
 
-    def work(self, input: str) -> Tuple[str, Optional[str]]:
+    def work(
+        self, request: DocumentRequest, context: ServicerContext
+    ) -> Tuple[str, Optional[str]]:
+        document = request.document
         cr = CountResults(
-            Bytes = len(input.encode()),
-            Lines = len(input.split("\n")) - 1,
-            Words = len(input.split("\n"))
+            Bytes=len(document),
+            Lines=len(str(document).split("\n")) - 1,
+            Words=len(str(document).split("\n")),
         )
         return (json.dumps(dataclasses.asdict(cr)), None)
+
 
 QDS_COUNT.run()
