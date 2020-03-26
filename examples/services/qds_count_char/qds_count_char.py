e03c074a3e4b011b70113a67b288f399a84a424f
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-03-26 16:03:22 +0100

    refactor(qds_count_char): adapt typing changes from services

--- examples/services/qds_count_char/qds_count_char.py
+++ examples/services/qds_count_char/qds_count_char.py
@@ -1,16 +1,19 @@
 from pathlib import Path
+from grpc import ServicerContext
+from service.proto.dtaservice_pb2 import DocumentRequest
 import sys
 from typing import Optional, Tuple
 
 sys.path.append(str(Path(".").resolve()))
 
-from dtaservice.server import DTAServer
+from service.server import DTAServer
 
 class QDS_COUNT_CHAR(DTAServer):
     version = "0.0.1"
     app_name = "QDS.ECHO"
 
-    def work(self, input: str) -> Tuple[str, Optional[str]]:
-        return (f"The document has {len(input)} characters.", None)
+    def work(self, request: DocumentRequest, context: ServicerContext) -> Tuple[str, Optional[str]]:
+        document = str(request.document)
+        return (f"The document has {len(document)} characters.", None)
 
 QDS_COUNT_CHAR.run()
