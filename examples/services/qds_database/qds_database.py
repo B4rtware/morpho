3453cfc6ce19ec09591cfc4465422664222fb990
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-03-26 16:58:14 +0100

    refactor(qds_database): partially resolve typing conflics

--- examples/services/qds_database/qds_database.py
+++ examples/services/qds_database/qds_database.py
@@ -1,27 +1,29 @@
 from pathlib import Path
+from grpc import ServicerContext
+from service.proto.dtaservice_pb2 import DocumentRequest
 import sys
 
 sys.path.append(str(Path(".").resolve()))
-from dtaservice.server import DTAServer
+from service.server import DTAServer
 
-import doctrans_py_swagger
+import doctrans_py_swagger_client
 
 from typing import Tuple, Optional
 
-api_instance = doctrans_py_swagger.TracesApi(doctrans_py_swagger.ApiClient())
+api_instance = doctrans_py_swagger_client.TracesApi(doctrans_py_swagger_client.ApiClient())
 
 
 class QDS_DATABASE(DTAServer):
     version = "0.0.1"
     app_name = "DE.TU-Berlin.QDS.DATABASE"
 
-    def work(self, request, context) -> Tuple[str, Optional[str]]:
-        trace = doctrans_py_swagger.Trace(service_name="")
+    def work(self, request: DocumentRequest, context: ServicerContext) -> Tuple[str, Optional[str]]:
+        trace = doctrans_py_swagger_client.Trace(service_name="")
 
         try:
             api_response = api_instance.traces_create(trace)
             print(api_response)
-        except doctrans_py_swagger.rest.ApiException as e:
+        except doctrans_py_swagger_client.rest.ApiException as e:
             # TODO: log this output
             print(f"Exception when calling TracesApi->traces_create: {e}\n")
 
