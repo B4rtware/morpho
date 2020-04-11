0b026d19794188dbb5567735e00a28a4d3a58739
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-04-11 21:28:29 +0200

    fix(examples.services.qds_echo): adapt breaking changes for the app settings

--- examples/services/qds_echo/qds_echo.py
+++ examples/services/qds_echo/qds_echo.py
@@ -1,22 +1,17 @@
 from pathlib import Path
 import sys
-from typing import Optional, Tuple
-from grpc import ServicerContext
 
 sys.path.append(str(Path(".").resolve()))
 
-from service.proto.dtaservice_pb2 import DocumentRequest
 from service.server import DTAServer
 
 
 class QDS_ECHO(DTAServer):
     version = "0.0.1"
-    app_name = "DE.TU-BERLIN.QDS.ECHO"
+    name = "DE.TU-BERLIN.QDS.ECHO"
 
-    def work(
-        self, request: DocumentRequest, context: ServicerContext
-    ) -> Tuple[str, Optional[str]]:
-        return (request.document.decode(), None)
+    def work(self, document: str) -> str:
+        return document
 
 
 QDS_ECHO.run()
