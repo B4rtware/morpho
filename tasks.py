e648347bcc33eb5d0baf4146a1b9153501a92545
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-04-19 14:56:13 +0200

    chore(invoke): add proto task and cleanup proto directory

--- tasks.py
+++ tasks.py
@@ -1,5 +1,12 @@
 from invoke import task
 
+from grpc_tools import protoc
+from pathlib import Path
+
+PROTO_PATH = Path("./service/proto/")
+PROTO_OUT_PATH = PROTO_PATH
+GRPC_PROTO_OUT_PATH = PROTO_PATH / Path("./grpc/")
+PROTO_FILE = PROTO_PATH / Path("./dtaservice.proto")
 
 @task
 def build(c):
@@ -19,3 +26,21 @@ def test(c):
 @task
 def format(c):
     c.run("poetry run python -m black --config=pyproject.toml --check .")
+
+@task
+def deps(c)
+
+
+@task
+def proto(c):
+    if not GRPC_PROTO_OUT_PATH.exists():
+        GRPC_PROTO_OUT_PATH.mkdir()
+    protoc.main(
+        (
+            "",
+            f"-I{PROTO_PATH.absolute()}",
+            f"--python_out={PROTO_OUT_PATH.absolute()}",
+            f"--grpc_python_out={GRPC_PROTO_OUT_PATH.absolute()}",
+            str(PROTO_FILE.name),
+        )
+    )
