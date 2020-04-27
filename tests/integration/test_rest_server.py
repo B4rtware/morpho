28c08fbc093d22df955aca35869c453c331e969f
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-04-27 18:34:50 +0200

    test(integration): adapt api changes

--- tests/integration/test_rest_server.py
+++ tests/integration/test_rest_server.py
@@ -1,5 +1,8 @@
 from base64 import b64encode
 from base64 import b64decode
+from morpho.config import BaseConfig
+from typing import Optional
+from morpho.rest.models import RawTransformDocumentPipeRequest, RawTransformDocumentResponse, TransformDocumentResponse
 import sys
 import pytest
 from threading import Thread
@@ -20,9 +23,14 @@ class MorphoTest(Server):
     name = "TEST"
     consumer = ["rest"]
 
-    def work(self, document: str) -> str:
+    def work(self, document: str, options: Optional[BaseConfig]) -> str:
         print("hellllllooo")
         return document
+
+
+class MorhpoPipeTest0(Server):
+    version = "0.0.1"
+    name = "TEST"
 
 
 @pytest.fixture(scope="module")
@@ -61,13 +69,19 @@ def test_rest_transform(rest_server):
 
     result = r.post(
         "http://127.0.0.1:50000/v1/qds/dta/document/transform",
-        json={"document": "Hello World!"},
+        json={
+            "document": "Hello World!",
+            "service_name": "QDS.TEST"
+        },
     )
 
     assert result.status_code == 200
     assert result.text
-    document_response = result.json()
-    assert document_response["trans_document"] == "Hello World!"
+    response: RawTransformDocumentResponse = result.json()
+    document_response = TransformDocumentResponse(document="", output=response["output"], error=response["error"])
+    document_response.document_b64 = response["document"]
+    assert document_response.document == "Hello World!"
+    assert document_response.error == []
 
 
 def test_rest_list(rest_server):
@@ -88,12 +102,6 @@ def test_rest_list(rest_server):
 
 
 # def test_rest_transform_pip():
-#     sys.argv = [sys.argv[0], "--protocols=rest"]
-
-#     app_thread = Thread(target=QDS_TEST.run, daemon=True)
-#     app_thread.start()
-
-#     app_thread.join(timeout=5)
-#     assert app_thread.is_alive()
-
-#     data = {"document": b64encode("Hello I want to be piped.")}
+#     data = RawTransformDocumentPipeRequest(
+#       document = b64encode("Hello I want to be piped.")
+#     )
