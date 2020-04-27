0dc506e37e1b70565149ca5e626a5242fa9c3c34
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-04-27 11:28:46 +0200

    feat(client): add transform pipe stub

--- morpho/client.py
+++ morpho/client.py
@@ -1,5 +1,6 @@
 from base64 import b64decode, b64encode
 from dataclasses import dataclass
+from morpho.rest.models import ServiceInfo, TransformDocumentPipeRequest, TransformDocumentPipeResponse
 from typing import List, Optional
 from urllib.error import HTTPError
 
@@ -8,14 +9,14 @@ from py_eureka_client.eureka_client import Application
 import requests
 
 from morpho.error import ServiceNotFoundError
-from morpho.server import Options
+from morpho.types import Options
 
 
 @dataclass
 class ClientConfig:
     registrar_url: str
 
-
+# TODO: consider to use models instead of function arguments e.g TransformDocumentRequest
 class Client:
     def __init__(self, config: ClientConfig) -> None:
         self.config = config
@@ -38,7 +39,7 @@ class Client:
         return None
 
     # TODO: consider to use **kwargs for options
-    def transform(
+    def transform_document(
         self,
         document: str,
         service_name: str,
@@ -74,8 +75,17 @@ class Client:
             ).decode("utf-8")
             return response_object
 
-    def transform_pipe(self):
-        pass
+    def transform_document_pipe(self, request: TransformDocumentPipeRequest) -> TransformDocumentPipeResponse:
+        # TODO: 
+        instance_address = self._get_instance_ip_address(request.services[0].name)
+        print(request.as_dict())
+        if instance_address:
+            response = requests.post(
+                f"http://{instance_address}/v1/qds/dta/document/transform-pipe",
+                json=request.as_dict()
+            )
+            print(response.text)
+            return TransformDocumentPipeResponse(**response.json())
 
     def list_services(self, service_name: str) -> List[str]:
         """Lists the services which are known by the provided service.
@@ -101,9 +111,20 @@ class Client:
 
 if __name__ == "__main__":
     config = ClientConfig(registrar_url="http://localhost:8761/eureka")
-    revolution = Client(config)
-    document = revolution.transform(
-        "Test Dokument", "DE.TU-BERLIN.QDS.ECHO", file_name="file.txt"
+    morpho_client = Client(config)
+    # document = morpho_client.transform_document(
+    #     "Test Dokument", "DE.TU-BERLIN.QDS.ECHO", file_name="file.txt"
+    # )
+    # print(c.list_services("DE.TU-BERLIN.QDS.ECHO"))
+    # print(document["trans_document"])
+
+    request = TransformDocumentPipeRequest(
+        document="Hello World",
+        file_name=None,
+        services=[
+            ServiceInfo(name="DE.TU-BERLIN.QDS.ECHOS", version="0.0.1", options=None),
+            ServiceInfo(name="DE.TU-BERLIN.QDS.ECHOS2", version="0.0.1", options=None),
+            ServiceInfo(name="DE.TU-BERLIN.QDS.ECHOS3", version="0.0.1", options=None)
+        ]
     )
-    print(c.list_services("DE.TU-BERLIN.QDS.ECHO"))
-    print(document["trans_document"])
+    morpho_client.transform_document_pipe(request=request)
