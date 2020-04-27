0069e7c277f09c87c71f6c2c8ad70c91a1eef7e7
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-04-27 11:35:18 +0200

    refactor: move some types into models

--- morpho/rest/models.py
+++ morpho/rest/models.py
@@ -1,19 +1,80 @@
 from abc import ABC, abstractmethod
-from base64 import b64decode
+from base64 import b64decode, b64encode
 import binascii
 from dataclasses import dataclass
 import json
-from typing import Any, Dict, List, Optional
+from morpho.types import Options
+# from morpho.config import BaseConfig
+import morpho.config as config
+from typing import Any, Dict, List, Optional, TypedDict
 
-from morpho.types import (
-    Options,
-    RawListResponse,
-    RawListService,
-    RawTransformDocumentPipeRequest,
-    RawTransformDocumentPipeResponse,
-    RawTransformDocumentRequest,
-    RawTransformDocumentResponse,
-)
+# from morpho.types import (
+#     Options,
+#     RawListServicesResponse,
+#     RawServiceInfo,
+#     RawTransformDocumentPipeRequest,
+#     RawTransformDocumentPipeResponse,
+#     RawTransformDocumentRequest,
+#     RawTransformDocumentResponse,
+# )
+
+
+# +-------------------------------------------------------------------------------------+
+# |                                  Raw Dict Types                                     |
+# +-------------------------------------------------------------------------------------+
+
+# consider to mvoe this into types again (or are they RawTypeModels?)
+
+class RawTransformDocumentResponse(TypedDict):
+    """Raw TransformDocumentResponse dict type"""
+
+    document: str
+    output: Optional[List[str]]
+    error: Optional[List[str]]
+
+
+class RawTransformDocumentRequest(TypedDict):
+    """Raw TransformDocumentRequest dict type"""
+
+    document: str
+    service_name: str
+    file_name: Optional[str]
+    options: Optional[Options]
+
+# class RawListService(TypedDict):
+#     """Raw ListService dict type"""
+
+#     name: str
+#     version: str
+#     options: Dict[str, Any]
+
+
+class RawServiceInfo(TypedDict):
+    """Raw ServiceInfo dict type"""
+
+    name: str
+    version: str
+    options: Optional[Dict[str, Any]]
+
+
+class RawListServicesResponse(TypedDict):
+    """Raw ListResponse dict type"""
+
+    services: List[RawServiceInfo]
+
+
+class RawTransformDocumentPipeRequest(TypedDict):
+    """Raw TransformDocumentPipeRequest dict type"""
+
+    document: str
+    file_name: Optional[str]
+    services: List[RawServiceInfo]
+
+
+class RawTransformDocumentPipeResponse(RawTransformDocumentResponse):
+    """Raw TransformDocumentPipeResponse dict type"""
+
+    sender: str
 
 # its models raw responses
 
@@ -54,19 +115,20 @@ class TransformDocumentRequest(BaseModel):
 
     @property
     def document(self) -> str:
-        return self._document
+        return b64decode(self._document.encode("utf-8")).decode("utf-8")
 
     @document.setter
     def document(self, document: str):
+        self._document = b64encode(document.encode("utf-8")).decode("utf-8")
         # validate base64
-        try:
-            b64decode(document, validate=True)
-            self._document = document
-        except binascii.Error:
-            # TODO: log this error
-            raise binascii.Error(
-                "Please make sure your document string is base64 encoded!"
-            )
+        # try:
+        #     b64decode(document, validate=True)
+        #     self._document = document
+        # except binascii.Error:
+        #     # TODO: log this error
+        #     raise binascii.Error(
+        #         "Please make sure your document string is base64 encoded!"
+        #     )
 
     def as_dict(self) -> RawTransformDocumentRequest:
         """Creates a dict of the object instance.
@@ -81,7 +143,7 @@ class TransformDocumentRequest(BaseModel):
             options=self.options,
         )
 
-
+# TODO: abstract it to a document variable which is inside another class so the validation process is only on one instance?
 class TransformDocumentResponse(BaseModel):
     def __init__(
         self,
@@ -96,19 +158,21 @@ class TransformDocumentResponse(BaseModel):
 
     @property
     def trans_document(self) -> str:
-        return self._trans_document
+        return b64decode(self._trans_document.encode("utf-8")).decode("utf-8")
 
     @trans_document.setter
-    def trans_document(self, trans_document: str) -> None:
+    def trans_document(self, trans_document: Optional[str]) -> None:
+        if trans_document is not None:
+            self._trans_document = b64encode(trans_document.encode("utf-8")).decode("utf-8")
         # validate base64
-        try:
-            b64decode(trans_document, validate=True)
-            self._trans_document = trans_document
-        except binascii.Error:
-            # TODO: log this error
-            raise binascii.Error(
-                "Please make sure your document string is base64 encoded!"
-            )
+        # try:
+        #     b64decode(trans_document, validate=True)
+        #     self._trans_document = trans_document
+        # except binascii.Error:
+        #     # TODO: log this error
+        #     raise binascii.Error(
+        #         "Please make sure your document string is base64 encoded!"
+        #     )
 
     def as_dict(self) -> RawTransformDocumentResponse:
         """Creates a dict of the object instance.
@@ -122,46 +186,70 @@ class TransformDocumentResponse(BaseModel):
             error=self.error,
         )
 
+@dataclass
+class ServiceInfo(BaseModel):
+    name: str
+    version: str
+    options: Optional[config.BaseConfig] = None
+
+    def as_dict(self) -> RawServiceInfo:
+        """Creates a dict of the object instance.
+
+        Returns:
+            RawServiceInfo: ServiceInfo as dict.
+        """
+        options = self.options.as_dict() if self.options else None
+        return RawServiceInfo(
+            name=self.name,
+            version=self.version,
+            options=options
+        )
 
 @dataclass
 class ListServicesResponse(BaseModel):
-    services: List[str]
+    services: List[ServiceInfo]
 
-    def as_dict(self) -> RawListResponse:
+    def as_dict(self) -> RawListServicesResponse:
         """Creates a dict of the object instance.
 
         Returns:
             RawListResponse: ListServicesResponse as dict.
         """
-        return RawListResponse(
-            services=[RawListService({"name": service}) for service in self.services]
+        return RawListServicesResponse(
+            services=[service.as_dict() for service in self.services]
         )
 
 
-class TransformDocumentPipeRequest(TransformDocumentRequest):
-    def __init__(
-        self,
-        document: str,
-        service_name: str,
-        services: List[Dict[str, Any]],
-        file_name: Optional[str] = None,
-        options: Optional[Options] = None,
-    ) -> None:
-        super().__init__(
-            document=document,
-            service_name=service_name,
-            file_name=file_name,
-            options=options,
-        )
+# TODO: implement base64 encode decode
+class TransformDocumentPipeRequest(BaseModel):
+    def __init__(self, document: str, services: List[ServiceInfo], file_name: Optional[str]) -> None:
+        self._document = ""
+        self.document = document
         self.services = services
+        self.file_name = file_name
+
+    @property
+    def document(self) -> str:
+        return b64decode(self._document.encode("utf-8")).decode("utf-8")
+
+    @document.setter
+    def document(self, document: str) -> None:
+        # validate base64
+        self._document = b64encode(document.encode("utf-8")).decode("utf-8")
+        # try:
+        #     b64decode(document, validate=True)
+        #     self._document = document
+        # except binascii.Error:
+        #     # TODO: log this error
+        #     raise binascii.Error(
+        #         "Please make sure your document string is base64 encoded!"
+        #     )
 
     def as_dict(self) -> RawTransformDocumentPipeRequest:
         return RawTransformDocumentPipeRequest(
             document=self.document,
-            service_name=self.service_name,
             file_name=self.file_name,
-            options=self.options,
-            services=self.services,
+            services=[info.as_dict() for info in self.services],
         )
 
 
--- morpho/types.py
+++ morpho/types.py
@@ -1,4 +1,5 @@
 from enum import Enum
+# from morpho.rest.models import ServiceInfo
 from typing import Any, Dict, List, NewType, Optional, TypedDict
 
 
@@ -10,62 +11,10 @@ class ServiceType(Enum):
     PROXY = "proxy"
     GATEWAY = "gateway"
 
+# @dataclass
+# class Metadata(ServiceInfo):
+#     def 
+
 # TODO consider to use dicttypes
 Options = NewType("Options", Dict[str, Any])
 Headers = NewType("Headers", Dict[str, str])
-
-
-# +-------------------------------------------------------------------------------------+
-# |                                  Raw Dict Types                                     |
-# +-------------------------------------------------------------------------------------+
-
-
-class RawTransformDocumentResponse(TypedDict):
-    """Raw TransformDocumentResponse dict type"""
-
-    document: str
-    output: Optional[List[str]]
-    error: Optional[List[str]]
-
-
-class RawTransformDocumentRequest(TypedDict):
-    """Raw TransformDocumentRequest dict type"""
-
-    document: str
-    service_name: str
-    file_name: Optional[str]
-    options: Optional[Options]
-
-
-class RawListService(TypedDict):
-    """Raw ListService dict type"""
-
-    name: str
-    version: str
-    options: Dict[str, Any]
-
-
-class RawServiceInfo(TypedDict):
-    """Raw ServiceInfo dict type"""
-
-    name: str
-    version: str
-    options: Optional[Dict[str, Any]]
-
-
-class RawListResponse(TypedDict):
-    """Raw ListResponse dict type"""
-
-    services: List[RawListService]
-
-
-class RawTransformDocumentPipeRequest(RawTransformDocumentRequest):
-    """Raw TransformDocumentPipeRequest dict type"""
-
-    services: List[Dict[str, Any]]
-
-
-class RawTransformDocumentPipeResponse(RawTransformDocumentResponse):
-    """Raw TransformDocumentPipeResponse dict type"""
-
-    sender: str
