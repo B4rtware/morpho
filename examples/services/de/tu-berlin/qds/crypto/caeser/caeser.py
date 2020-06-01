988eb5b89ef33310124e7f3908970d4ccf9e812e
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-06-01 16:58:17 +0200

    refactor(examples): change caeser example to use the new api

--- examples/services/de/tu-berlin/qds/crypto/caeser/caeser.py
+++ examples/services/de/tu-berlin/qds/crypto/caeser/caeser.py
@@ -1,17 +1,15 @@
+from morpho.server import Service
+from pydantic import BaseModel
 
-from dataclasses import dataclass
-from service.config import BaseConfig
-from service.server import DTAServer
-
-@dataclass
-class Options(BaseConfig):
+class Options(BaseModel):
     offset: int = 0
 
-class Caeser(DTAServer):
-    version = "0.0.1"
-    name = "de.tu-berlin.qds.crypto.caeser"
-    options = Options()
+service = Service(name="Caeser", version="0.0.1", options=Options)
 
-    def work(self, document: str, options: Options) -> str:
-        document = document.capitalize()
-        return "".join([chr(((ord(char) - 65 + options.offset) % 26) + 65) for char in document])
+@service.worker
+def work(document: str, options: Options) -> str:
+    document = document.capitalize()
+    return "".join([chr(((ord(char) - 65 + options.offset) % 26) + 65) for char in document])
+
+if __name__ == "__main__":
+    service.run()
