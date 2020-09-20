3bff7308850e37407cfaaa5c1fd7e760ccb2d170
B4rtware <34386047+B4rtware@users.noreply.github.com>
2020-09-20 16:10:07 +0200

    refactor(examples): update services to latest morpho version

--- examples/services/de/tu-berlin/qds/crypto/gateway/main.py
+++ examples/services/de/tu-berlin/qds/crypto/gateway/main.py
@@ -1,32 +1,9 @@
-from morpho.rest.raw import (
-    RawTransformDocumentPipeResponse,
-    RawTransformDocumentResponse,
-)
-from typing import Tuple, cast
+from morpho.types import DtaType
+from morpho.consumer import RestGatewayConsumer
 from morpho.server import Service
-from morpho.consumer import RestWorkConsumer
-from morpho.rest import Status
-from morpho.rest.models import TransformDocumentPipeRequest, TransformDocumentRequest
-import flask
-from morpho.log import logging as log
-
-
-class GatewayConsumer(RestWorkConsumer):
-
-    def _transform_document(self) -> Tuple[RawTransformDocumentResponse, Status]:
-        request_model = TransformDocumentRequest(**flask.request.json)
-        result = self.client.transform_document(**request_model.dict())
-        return cast(RawTransformDocumentResponse, result.dict()), Status.OK
-
-    def _transform_document_pipe(
-        self,
-    ) -> Tuple[RawTransformDocumentPipeResponse, Status]:
-        request_model = TransformDocumentPipeRequest(**flask.request.json)
-        result = self.client.transform_document_pipe(**request_model.dict())
-        return cast(RawTransformDocumentPipeResponse, result.dict()), Status.OK
 
 # TODO: add morpho tag attribute like tag=gateway or type=gateway
-gateway = Service(name="gateway", version="0.0.1", protocols=[GatewayConsumer])
+gateway = Service(name="crypto.gw", version="0.0.1", protocols=[RestGatewayConsumer], type=DtaType.GATEWAY)
 
 if __name__ == "__main__":
     gateway.run(50000)
